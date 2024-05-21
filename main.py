d6af22f7d7ec24161ae1db219062c04815919407
Dani <drahman@ucsd.edu>
2024-05-20 17:58:45 -0700

    gene list export

--- main.py
+++ main.py
@@ -7,4 +7,17 @@ file_path = "ecDNA Target genes.xlsx"
 df = pd.read_excel(file_path)
 
 # Display the first few rows of the DataFrame
-print(df.head())
+# Assuming the third column index is 2 (indexing starts from 0)
+third_column = df.iloc[:, 2]
+
+
+# Extract the text before the character "|"
+extracted_text = third_column.str.split('|').str[0]
+# Save the DataFrame to a CSV file
+extracted_text.to_csv('extracted_text.csv', index=False)
+
+# Create a new DataFrame with the extracted text
+
+
+# Display the first few rows of the new DataFrame
+print(extracted_text.head())
