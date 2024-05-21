efaa213a5f91c59567d6769cf5c6eb2cc3712069
Dani <drahman@ucsd.edu>
2024-05-20 19:29:04 -0700

    gene score df and file cleanup

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # genessentiality
 Using Depmap to test the essentiallity of genes impact on different cancer cell lines
+## Pipeline Description:
+### Obtaining data
+Given a list of list of gene data we extracted and filtered the particular genes ("~/filter.py") of interest from this file "~/ecDNA Target genes.xlsx". This extracted data further needed manual cleaning in the form of matching aliases with the primary name associated with a gene on Depmap "~/extracted_text.txt".
+
+This space seperated list is then used to generate a subdataset which can be downloaded from depmap's [download page](https://depmap.org/portal/download/custom/). 
+### Analyzing cell line data
+### Plotting
+## File Descriptions:
+### filter.py
+### ecDNA Target genes.xlsx
+### extracted_text.txt
--- filter.py
+++ filter.py
@@ -6,18 +6,17 @@ file_path = "ecDNA Target genes.xlsx"
 # Read the Excel file into a DataFrame
 df = pd.read_excel(file_path)
 
-# Display the first few rows of the DataFrame
 # Assuming the third column index is 2 (indexing starts from 0)
 third_column = df.iloc[:, 2]
 
-
 # Extract the text before the character "|"
 extracted_text = third_column.str.split('|').str[0]
-# Save the DataFrame to a CSV file
-extracted_text.to_csv('extracted_text.csv', index=False)
 
-# Create a new DataFrame with the extracted text
+# Convert the Series to a single string with space-separated values
+extracted_text_str = ' '.join(extracted_text.dropna().astype(str))
 
+# Save the string to a text file
+with open('extracted_text.txt', 'w') as f:
+    f.write(extracted_text_str)
 
-# Display the first few rows of the new DataFrame
-print(extracted_text.head())
+print("Genes extracted successfully.")
