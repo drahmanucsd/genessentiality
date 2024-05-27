01b33e25c1229fe9386ef98accc23af4a475d693
Dani <drahman@ucsd.edu>
2024-05-27 15:11:53 -0700

    cleanup

--- scripts/filter.py
+++ scripts/filter.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-# Specify the relative paths to the input files
+# Input files
 gene_list = "../Initial datasets/ecDNA Target genes.xlsx"
 raw_data = "../Initial datasets/aggregated_results.csv"
 depmap_file = '../DepMap-celllines.csv'
@@ -16,23 +16,20 @@ sample_col = raw_data_df.iloc[:, 1]
 class_col = raw_data_df.iloc[:, 4]
 gene_col = raw_data_df.iloc[:, 22]
 
-# Extract the text before the character "|" which contains the gene name
 gene_name = gene_column.str.split('|').str[0].tolist()
 
 # Create a DataFrame to store the match data
 match_data = []
 
-# Populate the match data
+# Match cell line ecDNA pos/neg
 for i in range(len(gene_name)):
     for j in range(len(sample_col)):
         if gene_name[i] in gene_col[j]:
             ecDNA_content = "pos" if class_col[j] == "ecDNA" else "neg"
             match_data.append([gene_name[i], sample_col[j], ecDNA_content])
 
-# Convert match data to a DataFrame
-match_df = pd.DataFrame(match_data, columns=['Gene Id', 'Cell Line', 'ecDNA Content'])
 
-# Extract the second column of each DataFrame
+match_df = pd.DataFrame(match_data, columns=['Gene Id', 'Cell Line', 'ecDNA Content'])
 match_cells = match_df['Cell Line']
 depmap_cells = depmap_df.iloc[:, 1]
 
@@ -45,23 +42,17 @@ for cell in match_cells:
     if not matched_row.empty:
         matched_rows = pd.concat([matched_rows, matched_row], ignore_index=True)
 
-# Extract matched IDs
 matched_ids = matched_rows.iloc[:, :1]
-
-# Concatenate match_df with matched_ids
 concatenated_df = pd.concat([match_df, matched_ids], axis=1)
 
 # Define headers
 headers = ['Gene Id', 'Cell Line', 'ecDNA Content', 'DP Cell Line ID']
 concatenated_df.columns = headers
 
-# Save the results to CSV files (optional)
+# Save the results to CSV files 
 matched_ids.to_csv('../output files/matched_ids.csv', index=False)
 concatenated_df.to_csv('../output files/match.csv', index=False)
 
-# Print the resulting DataFrame
-print("Matching rows have been saved to 'matched_ids.csv' and 'match.csv'")
-print(concatenated_df['DP Cell Line ID'])
 
-# Optional: Save DP Cell Line IDs to a separate file
-# concatenated_df['DP Cell Line ID'].to_csv('DP_cell_line_ID.csv', sep=',')
+print("Matching rows have been saved to 'matched_ids.csv' and 'match.csv'")
+
