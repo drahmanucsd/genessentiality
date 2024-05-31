492fab39165a68b3f723b60c7ced1fbec70c9ea5
YASHWINLM <sashimiym@gmail.com>
2024-05-30 17:42:57 -0700

    histogram instead of bar chart for gene plots

--- scripts/essen_dist.py
+++ scripts/essen_dist.py
@@ -1,20 +1,27 @@
-import pandas as pd, matplotlib.pyplot as plt, statistics as stats
+import pandas as pd
+import matplotlib.pyplot as plt
+import statistics as stats
 
-#path to cell ID chart and essentialiry scores
+# Path to cell ID chart and essentiality scores
 scp = "CRISPR_(DepMap_Public_23Q4+Score,_Chronos)_subsetted.csv"
 sc_df = pd.read_csv(scp)
-r,c = sc_df.shape
+r, c = sc_df.shape
 
-for i in range(1,c):
-    cid = list()
+# Loop through each column (excluding the first one, assuming it's the cell ID column)
+for i in range(1, c):
     gene = sc_df.columns[i]
     scs = sc_df[gene].dropna().tolist()
-    scs.sort()
-    mean, std = stats.mean(scs), stats.stdev(scs)
-    plt.bar(range(len(scs)),scs)
-    plt.xticks([])
+    
+    # Calculate mean and standard deviation
+    mean, std, med = stats.mean(scs), stats.stdev(scs), stats.median(scs)
+    
+    # Plot histogram
+    plt.hist(scs, bins=30, edgecolor='black')  # You can adjust the number of bins as needed
     plt.gca().invert_yaxis()
-    plt.title(gene + "\n" + str(mean)[:9] + "±" + str(std)[:9])
-    plt.ylabel("Essentiality Score")
-    plt.savefig("Plots/" + gene + ".png",format = 'png')
+    plt.title(gene + "\n" + str(med)[:9])
+    plt.xlabel("Essentiality Score")
+    plt.ylabel("Frequency")
+    
+    # Save the plot
+    plt.savefig("Plots/" + gene + ".png", format='png')
     plt.clf()
